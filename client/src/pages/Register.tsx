e156eec63ccfd6de5719c03c2697178616231f59
Niranjan R Soorej <niranjansoorej24bsc0117@iiitkottayam.ac.in@gmail.com>
2026-05-07 00:33:27 +0530

    Phase 17:3) Auth Pages

---
+++ client/src/pages/Register.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import API from "../api";
+
+function Register(){
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
+    const handleRegister = async () =>{
+        try{
+            await API.post("/users/register",{
+                name,
+                email,
+                password
+            });
+
+            alert("Registration successful");
+            window.location.href = "/login";
+        }catch(err){
+            console.log(err);
+            alert("Registration failed");
+        }
+    };
+
+    return (
+        <div style={{ padding: "20px" }}>
+            <h2>Registration</h2>
+
+            <input
+                type="text"
+                placeholder="Name"
+                onChange={(e) => setName(e.target.value)}
+            />
+            <br/>
+
+            <input
+                type="email"
+                placeholder="Email"
+                onChange={(e)=>setEmail(e.target.value)}
+            />
+            <br/>
+
+            <input
+                type="password"
+                placeholder="Password"
+                onChange={(e)=> setPassword(e.target.value)}
+            />
+
+            <button onClick={handleRegister}>
+                Register
+            </button>
+        </div>
+    );
+}
+
+export default Register;
