341ecd1f0abcdda3dc091903866c20669d1fc0b5
Niranjan R Soorej <niranjansoorej24bsc0117@iiitkottayam.ac.in@gmail.com>
2026-05-07 00:48:59 +0530

    Phase 17:4)Auth Pages linked to main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import App from "./App";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+import "./index.css";
+
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+
+        {/* Home */}
+        <Route path="/" element ={<App/>}/>
+        
+        {/* Login */}
+        <Route path="/login" element={<Login/>}/>
+
+        {/* Register */}
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
